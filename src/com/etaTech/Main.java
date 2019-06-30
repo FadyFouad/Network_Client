239e98a55052c12b9a22fdc00b8cbcfb155c84e6
fadyfouad <fady.fouad.a@gmail.com>
2019-06-30 23:48:16 +0200

    Auto Terminate Server

--- src/com/etaTech/Main.java
+++ src/com/etaTech/Main.java
@@ -5,30 +5,31 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.net.SocketTimeoutException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        try(Socket socket = new Socket("localhost",8081)){
+        try (Socket socket = new Socket("localhost", 8081)) {
+            socket.setSoTimeout(3000);
             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter writer = new PrintWriter(socket.getOutputStream(),true);
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
             Scanner scanner = new Scanner(System.in);
-            String echo ;
-            String response ;
-            do{
+            String echo;
+            String response;
+            do {
                 System.out.println("Enter Text");
                 echo = scanner.nextLine();
                 writer.println(echo);
 
-                if (!echo.equals("exit")){
+                if (!echo.equals("exit")) {
                     response = reader.readLine();
                     System.out.println(response);
                 }
             }
             while (!echo.equals("exit"));
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
+        }catch (SocketTimeoutException e){
+            System.out.println("Socket Time Out");
         } catch (IOException e) {
             e.printStackTrace();
         }
